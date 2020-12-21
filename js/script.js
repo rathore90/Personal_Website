297c6445de8692bb3cb6d53d531aab8df631a9fa
rathore90 <rathore90@hotmail.com>
2020-12-20 21:46:59 -0800

    wip

--- js/script.js
+++ js/script.js
@@ -18,4 +18,28 @@ $(document).ready(function(){
         $('.menu-btn .cross').toggleClass("active");
         $('.menu-btn .bars').toggleClass("hidden");
     });
+
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 1000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
